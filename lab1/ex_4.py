52680a0f23cc59727915dabb785d9bd826dc57d6
Zh_C <60555732+ZhTsai@users.noreply.github.com>
2021-03-19 14:13:27 +0000

    Update ex_4.py

--- lab1/ex_4.py
+++ lab1/ex_4.py
@@ -5,13 +5,15 @@ Created on Thu Jan 28 15:27:51 2021
 @author: 11647
 """
 
-# Given a list of numbers, return a list where adjacent identical elements are replaced by a single element.
+"""
+Given a list of numbers, return a list where adjacent identical elements are replaced by a single element.
 
-# For example,
+For example,
 
-# [1,2,2,3] yields [1,2,3]
-# [1,1,1,2,3,4,4,5] yields [1,2,3,4,5]
-# You may create a new list or modify the passed in list.
+[1,2,2,3] yields [1,2,3]
+[1,1,1,2,3,4,4,5] yields [1,2,3,4,5]
+You may create a new list or modify the passed in list.
+"""
 
 def remove_adjacent(nums): 
     new_list = nums[0:1]
